658a5fd221a4189cb7bf3b7f63f495037207f1fd
Vitoria Neris <v.a.mendes.da.silva@avanade.com>
2022-12-05 14:32:43 -0300

    Adicionar arquivos de projeto.

---
+++ PeopleRegistrationDatabaseAPI/Controllers/UserController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleRegistrationDatabaseAPI.Repositories;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PeopleRegistrationDatabaseAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private readonly UserRepository userRepository;
+
+        public UserController()
+        {
+            userRepository = new UserRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Query()
+        {
+            try
+            {
+                var listUser = userRepository.Listar();
+                return Ok(listUser);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        [Route("Query/{id}")]
+        public ActionResult Query(int id)
+        {
+            try
+            {
+                var listUser = userRepository.Query(id);
+                if (listUser.IdUser != 0)
+                {
+                    return Ok(listUser);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+        [HttpPost]
+
+        public ActionResult Create(Models.UserModel user)
+        {
+            try
+            {
+                userRepository.Insert(user);
+                return Ok(user);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Edit(Models.UserModel user)
+        {
+            try
+            {
+                userRepository.Update(user);
+                return Ok(user);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                userRepository.Delete(id);
+                return Ok();
+            }
+            catch (KeyNotFoundException)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ PeopleRegistrationDatabaseAPI/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace PeopleRegistrationDatabaseAPI.Models
+{
+    public class UserModel
+    {
+        public int IdUser { get; set; }
+        public String? UserName { get; set; }
+        public String? Address { get; set; }
+    }
+}
